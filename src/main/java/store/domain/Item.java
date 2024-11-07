4b4071dc716974bc1df182ee45702ab93efc2ba3
speculatingwook <bwook9908@gmail.com>
2024-11-08 00:45:01 +0900

    fix: change fields to promotion object

--- src/main/java/store/domain/Item.java
+++ src/main/java/store/domain/Item.java
@@ -5,28 +5,28 @@ import java.util.Objects;
 public class Item {
     private final String name;
     private final Integer price;
-    private final boolean isPromotion;
-    private final Integer promotionAmount;
+    private final Promotion promotion;
+    private final Integer promotionOfferCount;
 
-    public Item(String name, Integer price) {
-        validate(price);
+    public Item(String name, Integer price, Promotion promotion) {
+        validate(price, promotion);
         this.name = name;
         this.price = price;
-        this.isPromotion = false;
-        this.promotionAmount = 0;
+        this.promotion = promotion;
+        this.promotionOfferCount = promotion.getOfferCount();
     }
 
-    public Item(String name, Integer price, Integer promotionAmount) {
-        validate(price);
-        this.name = name;
-        this.price = price;
-        this.isPromotion = true;
-        validatePromotionAmount(promotionAmount);
-        this.promotionAmount = promotionAmount;
-    }
-
-    private void validate(Integer price) {
+    public Item(String name, Integer price){
         validatePrice(price);
+        this.name = name;
+        this.price = price;
+        this.promotion = null;
+        this.promotionOfferCount = 0;
+    }
+
+    private void validate(Integer price, Promotion promotion) {
+        validatePrice(price);
+        validatePromotionOffer(promotion.getOfferCount());
     }
 
     private void validatePrice(Integer price) {
@@ -35,33 +35,27 @@ public class Item {
         }
     }
 
-    private void validatePromotionAmount(Integer promotionAmount) {
+    private void validatePromotionOffer(Integer promotionAmount) {
         if (promotionAmount < 0) {
             throw new ArithmeticException("[CRITICAL] promotionAmount cannot be less than zero.");
         }
-    }
-
-    public boolean isLack(Integer requestQuantity) {
-        return promotionAmount < requestQuantity;
-    }
-
-    public Integer getLackQuantity(Integer requestQuantity) {
-        if(!isLack(requestQuantity)) {
-            return requestQuantity - promotionAmount;
-        }
-        return 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public boolean getIsPromotion() {
-        return isPromotion;
+    public Integer getPromotionOfferCount() {
+        return promotionOfferCount;
     }
+
+    public Integer getPrice() {
+        return price;
+    }
+
 
     @Override
     public int hashCode() {
-        return Objects.hash(name + isPromotion);
+        return Objects.hash(name + promotion);
     }
 }
