bad6956f48082f88c06890d93c9a222880bfba71
speculatingwook <bwook9908@gmail.com>
2024-11-07 21:23:54 +0900

    feat: implement feature that read file

---
+++ src/main/java/store/service/FileReader.java
@@ -0,0 +1,29 @@
+package store.service;
+
+import java.io.*;
+
+public class FileReader implements Reader {
+
+    @Override
+    public String read(String fileName) {
+        StringBuilder content = new StringBuilder();
+
+        try (InputStream inputStream = FileReader.class.getClassLoader().getResourceAsStream(fileName)) {
+            assert inputStream != null;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+
+            }
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return content.toString();
+    }
+
+}
