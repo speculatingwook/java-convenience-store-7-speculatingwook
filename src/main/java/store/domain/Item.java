10374dbd211cb30c28be3791f9098883091a6db7
speculatingwook <bwook9908@gmail.com>
2024-11-07 17:42:19 +0900

    feat: implement item that can save products

---
+++ src/main/java/store/domain/Item.java
@@ -0,0 +1,35 @@
+package store.domain;
+
+public class Item {
+    private final String name;
+    private final Integer price;
+    private Integer quantity;
+
+    public Item(String name, Integer price, Integer quantity) {
+        validate(price, quantity);
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    private void validate(Integer price, Integer quantity) {
+        validatePrice(price);
+        validateQuantity(quantity);
+    }
+
+    private void validateQuantity(Integer quantity) {
+        if (quantity < 0) {
+            throw new ArithmeticException("[ERROR] 수량은 음수가 불가능합니다.");
+        }
+    }
+
+    private void validatePrice(Integer price) {
+        if (price < 0) {
+            throw new ArithmeticException("[ERROR] 금액은 음수가 불가능합니다.");
+        }
+    }
+
+    public void reduceQuantity(Integer quantity) {
+        this.quantity = this.quantity - quantity;
+    }
+}
---
+++ src/test/java/store/domain/ItemTest.java
@@ -0,0 +1,7 @@
+package store.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+}
