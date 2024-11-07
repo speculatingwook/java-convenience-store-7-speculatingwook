f6d1b390b52e491b80b89cb996d018ccb6cd2e0e
speculatingwook <bwook9908@gmail.com>
2024-11-08 01:37:42 +0900

    feat: reimplement feature that the item is purchasable

--- src/main/java/store/domain/Inventory.java
+++ src/main/java/store/domain/Inventory.java
@@ -19,55 +19,35 @@ public class Inventory {
         }
     }
 
-    public boolean isLack(String name, Integer requestAmount, boolean isPromotion) {
-        Item targetItem = getItem(name, isPromotion);
-        return inventory.get(targetItem) < requestAmount;
+    public boolean isItemPurchasable(String item, int requestAmount){
+        return getTotalAmount(item) >= requestAmount;
     }
 
-    public void reduceAmount(String name, Integer requestAmount, boolean isPromotion) {
-        reduceAmountWhenStockIsSufficient(name, requestAmount, isPromotion);
-        reduceAmountWhenPromotionAndNotSufficient(name, requestAmount, isPromotion);
-        reduceAmountWhenNonPromotionAndNotSufficient(name, requestAmount, isPromotion);
+    public Integer getTotalAmount(String item){
+        Item itemWithPromotion = getItemWithPromotion(item);
+        Item itemWithoutPromotion = getItemWithoutPromotion(item);
+        return inventory.get(itemWithPromotion) + inventory.get(itemWithoutPromotion);
     }
 
-    private void reduceAmountWhenStockIsSufficient(String name, Integer requestAmount, boolean isPromotion) {
-        Item targetItem = getItem(name, isPromotion);
-        if(!isLack(name, requestAmount, isPromotion)) {
-            inventory.put(targetItem, inventory.get(targetItem) + requestAmount);
-        }
-    }
-
-    private void reduceAmountWhenPromotionAndNotSufficient(String name, Integer requestAmount, boolean isPromotion) {
-        Item targetItem = getItem(name, isPromotion);
-        if(isPromotion && isLack(name, requestAmount, isPromotion)) {
-            Integer lackAmount = requestAmount - inventory.get(targetItem);
-            inventory.put(targetItem, 0);
-            reduceAmountInNonPromotion(name, lackAmount);
-        }
-    }
-
-    private void reduceAmountWhenNonPromotionAndNotSufficient(String name, Integer requestAmount, boolean isPromotion) {
-        if(!isPromotion && isLack(name, requestAmount, isPromotion)) {
-            reduceAmountInNonPromotion(name, requestAmount);
-        }
-    }
-
-    private void reduceAmountInNonPromotion(String name, Integer requestAmount) throws ArithmeticException {
-        Item nonPromotionItem = getItem(name, false);
-        if(nonPromotionItem == null || isLack(name, requestAmount, false)) {
-            throw new ArithmeticException("[ERROR] 재고가 부족합니다.");
-        }
-        inventory.put(nonPromotionItem, inventory.get(nonPromotionItem) - requestAmount);
-    }
-
-    public Item getItem(String name, boolean isPromotion) {
+    public Item getItemWithPromotion(String itemName){
         for (Item item : inventory.keySet()) {
-            if(item.getName().equals(name) && item.getIsPromotion() == isPromotion) {
+            if(item.getName().equals(itemName) && item.isPromotionPresent()){
                 return item;
             }
         }
         return null;
     }
+
+    public Item getItemWithoutPromotion(String itemName){
+        for (Item item : inventory.keySet()) {
+            if(item.getName().equals(itemName) && !item.isPromotionPresent()){
+                return item;
+            }
+        }
+        return null;
+    }
+
+
 
     public Integer getInventoryAmount(Item item) {
         return inventory.get(item);
