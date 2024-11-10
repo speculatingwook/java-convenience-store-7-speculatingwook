587157784a0658bc2625114e578e5e279770f3ca
speculatingwook <bwook9908@gmail.com>
2024-11-11 01:25:33 +0900

    refactor: reduce code line under 10 in Application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -10,24 +10,43 @@ import store.pos.PosMachine;
 
 public class Application {
     public static void main(String[] args) {
-        init();
-        StoreView view = Container.getInstance(StoreView.class);
-        YesNoOption option = Container.getInstance(YesNoOption.class);
+        processTransaction();
+    }
 
-        PosMachine posMachine = Container.getInstance(PosMachine.class);
-        Payment payment = Container.getInstance(Payment.class);
+    private static void processTransaction() {
+        setup();
+        StoreComponents components = initializeComponents();
+        OrderItemInfo orderInfo = processOrder(components.posMachine, components.option);
+        String receipt = handlePayment(components.payment, orderInfo, components.option);
+        finalizeTransaction(components.view, receipt);
+    }
 
+    private static StoreComponents initializeComponents() {
+        return new StoreComponents(
+                Container.getInstance(StoreView.class),
+                Container.getInstance(YesNoOption.class),
+                Container.getInstance(PosMachine.class),
+                Container.getInstance(Payment.class)
+        );
+    }
+
+    private static OrderItemInfo processOrder(PosMachine posMachine, YesNoOption option) {
         posMachine.scanCartItems();
         posMachine.updateOrderItemInfo(option);
-        OrderItemInfo orderItemInfo = posMachine.getOrderItemInfo();
+        return posMachine.getOrderItemInfo();
+    }
 
-        payment.receiveOrderItemInfo(orderItemInfo);
+    private static String handlePayment(Payment payment, OrderItemInfo orderInfo, YesNoOption option) {
+        payment.receiveOrderItemInfo(orderInfo);
+        return payment.issueReceipt(option);
+    }
 
-        view.printReceipt(payment.issueReceipt(option));
+    private static void finalizeTransaction(StoreView view, String receipt) {
+        view.printReceipt(receipt);
         Container.reset();
     }
 
-    private static void init() {
+    private static void setup() {
         StoreConfig storeConfig = new StoreConfig();
         storeConfig.registerIO();
         storeConfig.registerStockServices();
@@ -36,4 +55,11 @@ public class Application {
         storeConfig.registerPaymentServices();
     }
 
+    private record StoreComponents(
+            StoreView view,
+            YesNoOption option,
+            PosMachine posMachine,
+            Payment payment
+    ) {
+    }
 }
