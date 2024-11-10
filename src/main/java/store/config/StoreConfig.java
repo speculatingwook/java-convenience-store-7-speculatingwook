5b77a696353f48135668082b36c1298c018ba677
speculatingwook <bwook9908@gmail.com>
2024-11-10 16:52:03 +0900

    refactor: implement feature registering core instances

--- src/main/java/store/config/StoreConfig.java
+++ src/main/java/store/config/StoreConfig.java
@@ -1,5 +1,30 @@
 package store.config;
 
+import store.io.reader.FileReader;
+import store.io.reader.Reader;
+import store.parser.ConvenienceStoreParser;
+import store.parser.Parser;
+import store.payment.discount.ConvenienceStoreDiscount;
+import store.payment.discount.Discount;
+import store.stock.Inventory;
+import store.stock.InventoryFactory;
+
 public class StoreConfig {
     private static final Integer MEMBERSHIP_DISCOUT_RATE = 30;
+
+    public void registerCoreServices() {
+        Container.register(Parser.class, ConvenienceStoreParser::new);
+        Container.register(Reader.class, FileReader::new);
+        Container.register(InventoryFactory.class, ()->
+                new InventoryFactory(Container.getInstance(Reader.class), Container.getInstance(Parser.class))
+        );
+        Container.register(Inventory.class, ()-> {
+            InventoryFactory inventoryFactory = Container.getInstance(InventoryFactory.class);
+            return inventoryFactory.createInventory();
+        });
+        Container.register(Discount.class, ()->
+                new ConvenienceStoreDiscount(MEMBERSHIP_DISCOUT_RATE, Container.getInstance(Inventory.class))
+        );
+    }
+
 }
